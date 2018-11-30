e3984b993daa9c50aa1364736758737aabb2f68a
Lena Ilina <ferr.98@mail.ru>
2018-12-01 02:26:54 +0300

    Second commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,6 +11,7 @@
 #include <QCryptographicHash>
 #include <fstream>
 
+//ХАХААХАХАХАХ ГИТ ЕТО ТАК ПРОСТА
 main_window::main_window(QWidget *parent)
         : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
